ab019bc0de15283dd8db32b29698fb381e2bc35c
zbailie <z.t.bailie@gmail.com>
2017-03-21 11:44:45 -0700

    Add files via upload

---
+++ Facial_Recognition/imageDBtoServer/imageDBtoServer/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace imageDBtoServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picID = new System.Windows.Forms.PictureBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxUserID = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picID
+            // 
+            this.picID.Image = ((System.Drawing.Image)(resources.GetObject("picID.Image")));
+            this.picID.Location = new System.Drawing.Point(12, 12);
+            this.picID.Name = "picID";
+            this.picID.Size = new System.Drawing.Size(258, 147);
+            this.picID.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picID.TabIndex = 0;
+            this.picID.TabStop = false;
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(273, 15);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(43, 13);
+            this.labelID.TabIndex = 1;
+            this.labelID.Text = "User ID";
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(273, 54);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(57, 13);
+            this.labelFirstName.TabIndex = 2;
+            this.labelFirstName.Text = "First Name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(272, 92);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(58, 13);
+            this.labelLastName.TabIndex = 3;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // textBoxUserID
+            // 
+            this.textBoxUserID.Location = new System.Drawing.Point(337, 12);
+            this.textBoxUserID.Name = "textBoxUserID";
+            this.textBoxUserID.Size = new System.Drawing.Size(142, 20);
+            this.textBoxUserID.TabIndex = 4;
+            this.textBoxUserID.TextChanged += new System.EventHandler(this.textBoxUserID_TextChanged);
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(336, 51);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(143, 20);
+            this.textBoxFirstName.TabIndex = 5;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(336, 89);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(143, 20);
+            this.textBoxLastName.TabIndex = 6;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(276, 127);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 7;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(404, 127);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Location = new System.Drawing.Point(276, 183);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisplay.TabIndex = 9;
+            this.buttonDisplay.Text = "Display";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            this.buttonDisplay.Click += new System.EventHandler(this.buttonDisplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 218);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.textBoxUserID);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.picID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picID)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picID;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxUserID;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonDisplay;
+    }
+}
+
---
+++ Facial_Recognition/imageDBtoServer/imageDBtoServer/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace imageDBtoServer
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source = mypc; Initial Catalog = imageDB; User ID = sa; Password = Cash2me!!");
+        SqlCommand command;
+        string imgLoc = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "JPG Files (*.jpg) |*.jpg| PNG Files (*.png)|*.png| All Files (*.*)|*.*";
+                dlg.Title = "Select User ID Image";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    imgLoc = dlg.FileName.ToString();
+                    picID.ImageLocation = imgLoc;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] img = null;
+                FileStream fs = new FileStream(imgLoc, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                img = br.ReadBytes((int)fs.Length);
+                string sql = "INSERT INTO Images(userID, firstName, lastName, ImageID)Values("+textBoxUserID.Text+",'"+textBoxFirstName.Text+"','"+textBoxLastName.Text+"', @img)";
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                command = new SqlCommand(sql, conn);
+                command.Parameters.Add(new SqlParameter("@img", img));
+                int x = command.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show(x.ToString() + " record(s) saved.");
+                textBoxUserID.Text = "";
+                textBoxFirstName.Text = "";
+                textBoxLastName.Text = "";
+                picID.Image = null;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "SELECT userID, firstName, lastName, ImageID FROM Images WHERE userID = "+textBoxUserID.Text+"";
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+                command = new SqlCommand(sql, conn);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                if(reader.HasRows)
+                {
+                    textBoxFirstName.Text = reader[1].ToString();
+                    textBoxLastName.Text = reader[2].ToString();
+                    byte[] img = (byte[])(reader[3]);
+                    if (img == null)
+                        picID.Image = null;
+                    else
+                    {
+                        MemoryStream ms = new MemoryStream(img);
+                        picID.Image = Image.FromStream(ms);
+                    }
+                }
+                else
+                {
+                    textBoxFirstName.Text = "";
+                    textBoxLastName.Text = "";
+                    picID.Image = null;
+                    MessageBox.Show("This does not Exist.");
+                }
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBoxUserID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
